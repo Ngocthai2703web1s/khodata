8fa516306dbdcda947377ace610e02745f322f9e
Ngocthai2703web1s <159770013+Ngocthai2703web1s@users.noreply.github.com>
2024-02-22 08:31:14 +0700

    Update xxx.js

--- xxx.js
+++ xxx.js
@@ -1,18 +1,18 @@
 const xxx = [
     {
-        src: 'xxx1.jpg',
+        src: 'https://vcdn1-giaitri.vnecdn.net/2022/03/31/shawshank-redemption-137394414-7901-5918-1648713782.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=-66ReGIzmqsqLLHhizjURw',
         alt: 'XXX 1',
         caption: 'XXX 1',
         link: '#'
     },
     {
-        src: 'xxx2.jpg',
+        src: 'https://vcdn1-giaitri.vnecdn.net/2022/03/31/shawshank-redemption-137394414-7901-5918-1648713782.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=-66ReGIzmqsqLLHhizjURw',
         alt: 'XXX 2',
         caption: 'XXX 2',
         link: '#'
     },
     {
-        src: 'xxx3.jpg',
+        src: 'https://vcdn1-giaitri.vnecdn.net/2022/03/31/shawshank-redemption-137394414-7901-5918-1648713782.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=-66ReGIzmqsqLLHhizjURw',
         alt: 'XXX 3',
         caption: 'XXX 3',
         link: '#'

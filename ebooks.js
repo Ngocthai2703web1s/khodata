35401cfd2cdc8b4162e9b982afe3da26a68fad62
Ngocthai2703web1s <159770013+Ngocthai2703web1s@users.noreply.github.com>
2024-02-22 08:29:52 +0700

    Update ebooks.js

--- ebooks.js
+++ ebooks.js
@@ -1,18 +1,18 @@
 const ebooks = [
     {
-        src: 'ebook1.jpg',
+        src: 'https://vcdn1-giaitri.vnecdn.net/2022/03/31/shawshank-redemption-137394414-7901-5918-1648713782.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=-66ReGIzmqsqLLHhizjURw',
         alt: 'Ebook 1',
         caption: 'Clean Code: A Handbook of Agile Software Craftsmanship',
         link: 'https://www.goodreads.com/book/show/3735293-clean-code'
     },
     {
-        src: 'ebook2.jpg',
+        src: 'https://vcdn1-giaitri.vnecdn.net/2022/03/31/shawshank-redemption-137394414-7901-5918-1648713782.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=-66ReGIzmqsqLLHhizjURw',
         alt: 'Ebook 2',
         caption: 'The Pragmatic Programmer: Your Journey to Mastery',
         link: 'https://www.goodreads.com/book/show/4099.The_Pragmatic_Programmer'
     },
     {
-        src: 'ebook3.jpg',
+        src: 'https://vcdn1-giaitri.vnecdn.net/2022/03/31/shawshank-redemption-137394414-7901-5918-1648713782.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=-66ReGIzmqsqLLHhizjURw',
         alt: 'Ebook 3',
         caption: 'Design Patterns: Elements of Reusable Object-Oriented Software',
         link: 'https://www.goodreads.com/book/show/85009.Design_Patterns'

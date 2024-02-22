0319b686f4a92892899f9f4edaee18efdeffae48
Ngocthai2703web1s <159770013+Ngocthai2703web1s@users.noreply.github.com>
2024-02-22 08:30:19 +0700

    Update games.js

--- games.js
+++ games.js
@@ -1,18 +1,18 @@
 const games = [
     {
-        src: 'game1.jpg',
+        src: 'https://vcdn1-giaitri.vnecdn.net/2022/03/31/shawshank-redemption-137394414-7901-5918-1648713782.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=-66ReGIzmqsqLLHhizjURw',
         alt: 'Game 1',
         caption: 'The Legend of Zelda: Breath of the Wild',
         link: 'https://www.nintendo.com/games/detail/the-legend-of-zelda-breath-of-the-wild-switch/'
     },
     {
-        src: 'game2.jpg',
+        src: 'https://vcdn1-giaitri.vnecdn.net/2022/03/31/shawshank-redemption-137394414-7901-5918-1648713782.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=-66ReGIzmqsqLLHhizjURw',
         alt: 'Game 2',
         caption: 'Red Dead Redemption 2',
         link: 'https://www.rockstargames.com/reddeadredemption2/'
     },
     {
-        src: 'game3.jpg',
+        src: 'https://vcdn1-giaitri.vnecdn.net/2022/03/31/shawshank-redemption-137394414-7901-5918-1648713782.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=-66ReGIzmqsqLLHhizjURw',
         alt: 'Game 3',
         caption: 'The Witcher 3: Wild Hunt',
         link: 'https://thewitcher.com/en/witcher3'

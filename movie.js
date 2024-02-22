93993929e94cb091fd04f8da28fb8ed1b8adceea
Ngocthai2703web1s <159770013+Ngocthai2703web1s@users.noreply.github.com>
2024-02-22 08:28:58 +0700

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,18 +1,18 @@
 const movies = [
     {
-        src: 'movie1.jpg',
+        src: 'https://vcdn1-giaitri.vnecdn.net/2022/03/31/shawshank-redemption-137394414-7901-5918-1648713782.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=-66ReGIzmqsqLLHhizjURw',
         alt: 'Movie 1',
         caption: 'The Shawshank Redemption',
         link: 'https://www.imdb.com/title/tt0111161/'
     },
     {
-        src: 'movie2.jpg',
+        src: 'https://vcdn1-giaitri.vnecdn.net/2022/03/31/shawshank-redemption-137394414-7901-5918-1648713782.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=-66ReGIzmqsqLLHhizjURw',
         alt: 'Movie 2',
         caption: 'The Godfather',
         link: 'https://www.imdb.com/title/tt0068646/'
     },
     {
-        src: 'movie3.jpg',
+        src: 'https://vcdn1-giaitri.vnecdn.net/2022/03/31/shawshank-redemption-137394414-7901-5918-1648713782.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=-66ReGIzmqsqLLHhizjURw',
         alt: 'Movie 3',
         caption: 'The Dark Knight',
         link: 'https://www.imdb.com/title/tt0468569/'
